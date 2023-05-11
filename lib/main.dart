87cc712af703ece272962c33d0cb5bb8d8f5ed02
kimtaeyoon20031220 <k0278kim@gmail.com>
2023-05-11 17:12:14 +0900

    업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              '한글:',
+              '한글?:',
             ),
             Text(
               '$_counter',
